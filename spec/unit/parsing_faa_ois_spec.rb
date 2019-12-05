da19dc40f199bda32c939326696a64302ebe32fe
Carlos Nunez <carlosonunez@noreply.github.com>
2019-12-05 15:25:26 -0600

    Let's begin by distiling the page into something simpler

---
+++ spec/unit/parsing_faa_ois_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Given the FAA OIS page" do
+  context "When I fetch it" do
+    example "It condenses the page into a really nice YAML file" do
+      expected_output = YAML.load(File.read('spec/fixtures/example_parsed_ois_info.yml'))
+      actual_output = FAAOISAPI::Page.parse
+      [ :programs, :ground_stops, :delays, :closures ].each do |section|
+        expect(actual_output[section]).to eq expected_output[section]
+      end
+    end
+  end
+end
