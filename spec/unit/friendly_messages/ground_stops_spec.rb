2d9d86addbd32f8cb14c851cf8cdf5058c3e90e2
Carlos Nunez <carlosonunez@noreply.github.com>
2019-12-12 19:10:41 -0500

    Avoid big specs by organizing this

--- spec/unit/friendly_messages/ground_stops_spec.rb
+++ spec/unit/friendly_messages/ground_stops_spec.rb
@@ -1,20 +1,31 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
+# lots of mocking needed here
+# rubocop:disable Metrics/BlockLength
 describe 'Given FAA OIS data' do
+  let(:example_output) do
+    YAML.safe_load(File.read('spec/fixtures/example_parsed_ois_info.yml'),
+                   symbolize_names: true)
+  end
+  before(:each) do
+    allow(FAAOISAPI).to receive(:parse).and_return(example_output)
+    allow(FAAOISAPI::Airport).to receive(:new)
+      .and_return(double(FAAOISAPI::Airport,
+                         full_name: 'Newark-Liberty International',
+                         iata: 'EWR'))
+  end
+
   context 'When a national program is in place' do
+    let(:ewr_ground_stop) do
+      example_output[:programs].find do |program|
+        program[:airport_or_zone_name] == 'EWR'
+      end
+    end
+
     context 'And no other areas are affected' do
       example 'Then I get a friendly message describing it', :unit do
-        allow(FAAOISAPI::Airport).to receive(:new)
-          .and_return(double(FAAOISAPI::Airport,
-                             full_name: 'Newark-Liberty International',
-                             iata: 'EWR'))
-        example_output =
-          YAML.safe_load(File.read('spec/fixtures/example_parsed_ois_info.yml'),
-                         symbolize_names: true)
-        allow(FAAOISAPI).to receive(:parse).and_return(example_output)
-        ewr_ground_stop =
-          example_output[:programs]
-          .find { |program| program[:airport_or_zone_name] == 'EWR' }
         ewr_ground_stop[:areas_affected] = []
         expected_friendly_message = <<~FRIENDLY_MESSAGE.chomp
           Newark-Liberty International (EWR) is currently experiencing a ground \
@@ -29,3 +40,4 @@ describe 'Given FAA OIS data' do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
